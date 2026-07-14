24255df800e79d5ab2a5b51816a8145f550c2ba4
SenSer3 <armanpatel3407@gmail.com>
2026-07-14 18:23:37 +0530

    solved

---
+++ Watermelon.java
@@ -0,0 +1,16 @@
+// Codeforces Problem 4A - Watermelon
+
+import java.util.Scanner;
+
+public class Watermelon {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int weight = scan.nextInt();
+        scan.close();
+        if (weight>2 && weight%2==0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
