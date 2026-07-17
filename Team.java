351dcf9918ef9cffbba17c418dcbe79298ec493e
SenSer3 <armanpatel3407@gmail.com>
2026-07-17 15:51:36 +0530

    solved

---
+++ Team.java
@@ -0,0 +1,22 @@
+// Codeforces Problem 231A - A Team
+import java.util.Scanner;
+
+public class Team {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int prob = sc.nextInt();
+        int count = 0;
+        for(int i=0;i<prob;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if((a+b+c)>=2){
+                count++;
+            }
+        }
+        System.out.println(count);
+        
+        sc.close();
+
+    }
+}
